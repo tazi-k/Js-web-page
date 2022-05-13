6ba70ea48341ee73f8278ac162ce881017d362ab
tazi-k <tajitoc14@gmail.com>
2022-05-14 03:53:20 +0900

    肉球パンチ

--- Js_web_page.js
+++ Js_web_page.js
@@ -1,50 +1,71 @@
-function clock(){
-  var twoDigit = function(num){
-    var digit;
-    if(num < 10){
-      digit = "0" + num;
-    }else{
-      digit = num;
+window.onload = function(){
+
+  //以下ボタン
+  
+  var nikukyu_btn = document.getElementById("nikukyu_btn");
+  
+  var nikukyu = document.getElementById("test");
+  console.log(nikukyu);
+  
+  function nikukyuAppear() {
+    nikukyu.style.opacity = 1;
+    window.setTimeout(nikukyuHide, 2000);
+  }
+  
+  function nikukyuHide() {
+    nikukyu.style.opacity = 0;
+  }
+  
+  nikukyu_btn.addEventListener('click', nikukyuAppear);
+  
+  function clock(){
+    var twoDigit = function(num){
+      var digit;
+      if(num < 10){
+        digit = "0" + num;
+      }else{
+        digit = num;
+      }
+  
+      return digit;
     }
-
-    return digit;
-  }
-
-  // 現在日時のインスタンス取得
-  var now = new Date();
   
-  var weeks = new Array("Sun","Mon","Thu","Wed","Thu","Fri","Sat"); 
+    // 現在日時のインスタンス取得
+    var now = new Date();
+    
+    var weeks = new Array("Sun","Mon","Thu","Wed","Thu","Fri","Sat"); 
+    
+    var year = now.getFullYear();
+    var month = twoDigit(now.getMonth() + 1);
+    var day = twoDigit(now.getDate());
+    var week = weeks[now.getDay()];
+    var hour = twoDigit(now.getHours());
+    var minute = twoDigit(now.getMinutes());
+    var second = twoDigit(now.getSeconds());
   
-  var year = now.getFullYear();
-  var month = twoDigit(now.getMonth() + 1);
-  var day = twoDigit(now.getDate());
-  var week = weeks[now.getDay()];
-  var hour = twoDigit(now.getHours());
-  var minute = twoDigit(now.getMinutes());
-  var second = twoDigit(now.getSeconds());
-
-  document.getElementById("clock_time").textContent = hour + ":" + minute + ":" + second;
-  document.getElementById("clock_date").textContent = year + "/" + month + "/" + day + " (" + week + ")";
-}
-
-setInterval(clock, 1000);
-
-//以下スライドショー
-
-var indexValue = 0;
-function slideShow(){
-  setTimeout(slideShow,2500);
-  var x;
-  const img = document.querySelectorAll("img");
-  for(x=0; img.length>x; x++){
-    img[x].style.display = "none";
+    document.getElementById("clock_time").textContent = hour + ":" + minute + ":" + second;
+    document.getElementById("clock_date").textContent = year + "/" + month + "/" + day + " (" + week + ")";
   }
-  indexValue++;
-  //全ての画像を一巡したらカウントリセット
-  if(indexValue > img.length){
-    indexValue = 1;
+  
+  setInterval(clock, 1000);
+  
+  //以下スライドショー
+  
+  var indexValue = 0;
+  function slideShow(){
+    setTimeout(slideShow,2500);
+    var x;
+    const img = document.querySelectorAll("img.img_content:not(.nikukyu)");
+    for(x=0; img.length>x; x++){
+      img[x].style.display = "none";
+    }
+    indexValue++;
+    //全ての画像を一巡したらカウントリセット
+    if(indexValue > img.length){
+      indexValue = 1;
+    }
+    //配列の最初は「0」なので「-1」、一つの画像だけblock要素にして表示
+    img[indexValue - 1].style.display = "inline-block";
   }
-  //配列の最初は「0」なので「-1」、一つの画像だけblock要素にして表示
-  img[indexValue - 1].style.display = "inline-block";
+  slideShow();
 }
-slideShow();
